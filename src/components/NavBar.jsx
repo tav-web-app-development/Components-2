7f5ef75b19718e59f4a7826cff694f880b0f690e
Razieh <razi.benvidi98@gmail.com>
2024-02-20 19:31:08 -0500

    Done (1-4)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,18 @@
-export default function NavBar() {
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+export default function NavBar({ User }) {
+  let txt = "";
+  if (User) {
+    txt = ` Welcome ${User.firstname} ${User.lastname}`;
+  } else {
+    txt = "Please Login!";
+  }
   return (
     <>
+      <h3>{txt}</h3>
+      <h4>
+        {User && User.itemsInCart != 0 && `${User.itemsInCart} in your cart.`}
+      </h4>
       <a href="#home">Home </a>
       <a href="#home">Laptops </a>
       <a href="#contact">Contact </a>
---
+++ src/constant/User.jsx
@@ -0,0 +1,10 @@
+const User = {
+  firstname: "Razieh",
+  lastname: "Esmaeilibenvidi",
+  email: "Razi.benvidi98@gmail.com",
+  imgurl:
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3D%2522user%2Bicon%2522&psig=AOvVaw0yErEo4DtIrTiy47lqhA7B&ust=1708554343593000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNj3kYn7uoQDFQAAAAAdAAAAABAE",
+  itemsInCart: 0,
+};
+
+export { User };
