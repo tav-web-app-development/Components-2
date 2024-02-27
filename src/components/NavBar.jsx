bfb3e0672717e45250d7e5e0a2eadde9423beb14
Nestor Gomez <neragoro95@gmail.com>
2024-02-27 17:59:20 -0500

    changes Done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,19 @@
-export default function NavBar() {
+export default function NavBar({user}) {
+  
   return (
     <>
+      {user ? (
+        <p>Welcome, {user.firstName}!</p>
+      ) : (
+        <p>Login</p>
+      )}
+      {user && user.itemsInCart > 0 && (
+        <p>{user.itemsInCart} in your cart</p>
+      )}
       <a href="#home">Home </a>
       <a href="#home">Laptops </a>
       <a href="#contact">Contact </a>
-      <a href="#about">About </a>
+      <a href="#about">About </a>       
     </>
-  );
-}
+  );  
+};
