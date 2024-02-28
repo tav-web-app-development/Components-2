2a3c759686dba6ed917e984b4616618248cd0131
gurleen <gurleenkaurkhurpa19@gmail.com>
2024-02-27 19:16:50 -0500

    assignment3 completed

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,19 @@
 /* eslint-disable react/prop-types */
 export default function ProductCard({ product }) {
-  console.log(product.description.length);
-
   return (
     <>
       <div style={{ border: "2px solid red" }}>
         <img src={product.imageUrl} alt="hp 15" />
         <h3>{product.name}</h3>
-        <p>
-          {product.description.length > 70
-            ? product.description.slice(0, 69) + "..."
-            : product.description}
-        </p>
-        <div className="price">${product.price}</div>
+        <p>{product.description}</p>
+        <div className="price" onMouseOver={() => console.log(product.price)}>
+          ${product.price}
+        </div>
         <h4>{product.isInStock ? "In Stock" : "Unavailable"}</h4>
 
-        <button>Add to Cart</button>
+        <button onClick={() => alert(`You added ${product.name}`)}>
+          Add to Cart
+        </button>
         <br />
       </div>
     </>
